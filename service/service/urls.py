b3925ba85e5b8a5028fbef6a581b0912ecf9d24c
MaxiMy <maximy.pro@gmail.com>
2022-03-09 16:15:39 +0300

    update project

--- service/service/urls.py
+++ service/service/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from authors.views import AuthorModelViewSet
-from users.views import UserModelViewSet
+# from authors.views import AuthorModelViewSet,BiographyModelViewSet,BookModelViewSet,ArticleModelViewSet
+from users.views import UserModelViewSet,ProjectModelViewSet,TodoListModelViewSet
 
 router = DefaultRouter()
-router.register('authors', AuthorModelViewSet)
-router.register('users', UserModelViewSet)
+# router.register('authors', AuthorModelViewSet)
+# router.register('biographies',BiographyModelViewSet)
+# router.register('books', BookModelViewSet)
+# router.register('articles', ArticleModelViewSet)
+# router.register('users', UserModelViewSet)
+router.register('users',UserModelViewSet)
+router.register('projects',ProjectModelViewSet)
+router.register('todolists',TodoListModelViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
