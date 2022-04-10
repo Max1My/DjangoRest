168d6e264a4752841180427a409e6797467bc7db
Maksim <57528691+Max1My@users.noreply.github.com>
2022-04-10 22:17:05 +0300

    up

--- service/authors/tests.py
+++ service/authors/tests.py
@@ -1,4 +1,6 @@
 import json
+from pprint import pprint
+
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import (
@@ -10,8 +12,8 @@ from rest_framework.test import (
 )
 # from mixer.backend.django import mixer
 from django.contrib.auth.models import User
-from service.authors.views import AuthorViewSet
-from service.authors.models import Author,Book
+from authors.views import AuthorViewSet
+from authors.models import Author,Book
 
 
 class TestAuthorViewSet(TestCase):
@@ -20,4 +22,25 @@ class TestAuthorViewSet(TestCase):
         request = factory.get('/api/authors/')
         view = AuthorViewSet.as_view({'get':'list'})
         response = view(request)
-        self.assertEqual(response.status_code,status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        """Тест на создания автора анонимным пользователем"""
+        factory = APIRequestFactory()
+        request = factory.post('/api/authors/',
+                               {'name':'Пушкин','birthday_year':1799} ,
+                               format='json')
+        view = AuthorViewSet.as_view({'get': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_author_for_admin(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/authors/',
+                               {'name': 'Пушкин', 'birthday_year': 1799},
+                               format='json')
+        admin = User.objects.create_superuser('maximy','mksadmin@gmail.com','sync')
+        force_authenticate(request,admin)
+        view = AuthorViewSet.as_view({'get': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- service/authors/views.py
+++ service/authors/views.py
@@ -5,19 +5,19 @@ from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
 class AuthorViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
 
-    def create(self,request,*args,**kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        headers['bababa'] = ''
-        return Response(serializer.data,status=status.HTTP_201_CREATED, headers=headers)
+    # def create(self,request,*args,**kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     headers = self.get_success_headers(serializer.data)
+    #     headers['bababa'] = ''
+    #     return Response(serializer.data,status=status.HTTP_201_CREATED, headers=headers)
 
 class BookViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
     serializer_class = BookSerializer
     queryset = Book.objects.all()
